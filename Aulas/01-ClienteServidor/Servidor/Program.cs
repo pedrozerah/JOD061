170bc815892fdc05415ec3a5b6f3305b21d8855a
Pedro Fernandes <pedrofcnovaes@gmail.com>
2020-10-16 22:03:00 -0300

    Entrega da Atividade 2

---
+++ Aulas/01-ClienteServidor/Servidor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Servidor
+{
+    class Program
+    {
+        const int porta = 7000;
+
+        static void Main(string[] args)
+        {
+            TcpListener servidor = new TcpListener(IPAddress.Any, porta);
+
+            try
+            {
+                Console.WriteLine("Servidor no ar!");
+                Console.WriteLine("Para sair, pressione CTRL+C.");
+                servidor.Start();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Deu ruim! Não subiu o servidor");
+            }
+
+            while (true)
+            {
+                TcpClient cliente = servidor.AcceptTcpClient();
+                Console.WriteLine("Conexao estabelecida!");
+                
+                byte[] bytes = new byte[1024];
+                NetworkStream stream = cliente.GetStream();
+
+                while (true)
+                {
+                    int bytesRead = stream.Read(bytes, 0, bytes.Length);
+
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("Mensagem recebida: {0}", Encoding.ASCII.GetString(bytes, 0, bytesRead));
+                }
+
+                stream.Close();
+                cliente.Close();
+                Console.WriteLine("Conexao encerrada!");
+            }
+        }
+    }
+}
